7756c80927bca2fd37c1aec662d6dd55979ea12d
eliptik <adamfeyk5@gmail.com>
2022-11-25 21:58:58 +0300

    Create operate function with switch statement

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
 
-function subtrack(a,b){
-    return a-b
+function subtrack(a, b) {
+    return a - b
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
 
-function divide(a,b){
-    return a/b
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b)
+            break;
+        case "-":
+            return subtrack(a, b)
+            break;
+        case "*":
+            return multiply(a, b)
+            break;
+        case "/":
+            return divide(a, b)
+            break;
+        default:
+            return add(a, b)
+            break;
+    }
 }
