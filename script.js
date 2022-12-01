2a9e98704fc0e4be8b9b965427165ca3e6dfd1d7
eliptik <adamfeyk5@gmail.com>
2022-12-01 20:39:51 +0300

    Additiona WIP

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function add(a, b) {
-    return a + b
+    //display.textContent = Number(a)+Number(b)
+    result = Number(a) + Number(b)
+    //number1 = result
+    //number2 = null
+    resultDisplay.textContent = result
+    console.log("number1: " + number1)
+    console.log("number2: " + number2)
+    console.log("result: " + result)
+    //operator = ""
 }
 
 function subtrack(a, b) {
@@ -28,14 +36,11 @@ function operate(a, operator, b) {
         case "/":
             return divide(a, b)
             break;
-        default:
-            return add(a, b)
-            break;
     }
 }
 
 const display = document.querySelector(".screen")
-
+const resultDisplay = document.querySelector(".result")
 
 const btn9 = document.querySelector("#btn9")
 const btn8 = document.querySelector("#btn8")
@@ -47,63 +52,251 @@ const btn3 = document.querySelector("#btn3")
 const btn2 = document.querySelector("#btn2")
 const btn1 = document.querySelector("#btn1")
 const btn0 = document.querySelector("#btn0")
-
 const btnAC = document.querySelector("#btnAC")
+const btnEquals = document.querySelector("#btnEquals")
+const btnAdd = document.querySelector("#btnAdd")
 
-let displayValue = ""
+let displayValue = display.textContent // ""
+let operator = ""
+//let values = []
+//let result = []
+let number1 = null
+let number2 = null
+let result = null
 
-btn9.addEventListener("click", ()=> {
-    display.textContent += 9
-    displayValue += "9"
+let firstActive = true
+let secondActive = false
+let thirdActive = false
+
+let override = false
+let isCalculated = false
+
+btnAdd.addEventListener("click", () => {
+    operator = "+"
+
+    if (firstActive && displayValue != "") {
+        number1 = Number(displayValue)
+
+        resultDisplay.textContent = number1 + " + "
+        //display.textContent = ""
+        displayValue = ""
+        console.log("number1: " + number1)
+        console.log("number2: " + number2)
+        console.log("operator: " + operator)
+        console.log("result: " + result)
+        firstActive = false
+        secondActive = true
+        override = true
+    }
+    else if (secondActive && displayValue != "") {
+        number2 = Number(displayValue)
+        //display.textContent = ""
+        displayValue = ""
+        operate(number1, operator, number2)
+        resultDisplay.textContent += " + "
+        console.log("number1: " + number1)
+        console.log("number2: " + number2)
+        secondActive = false
+        thirdActive = true
+        override = true
+    }
+    else if (thirdActive && displayValue != "") {
+        number1 = result
+        number2 = Number(displayValue)
+        displayValue = ""
+        operate(number1, operator, number2)
+        resultDisplay.textContent = result + " + "
+        override = true
+    }
+
+
+    /* if(values.length < 2 && displayValue != ""){
+    operator = "+"
+    values.push(Number(displayValue))
+    display.textContent = ""
+    displayValue = ""
+    console.log(values)
+    } else if(values.length == 2){
+        operate(values[0],operator,values[1])
+        console.log(values)
+        values = []   
+    } */
+
 })
 
-btn8.addEventListener("click", ()=> {    
-    display.textContent += 8
-    displayValue += "8"
+btnEquals.addEventListener("click", () => {
+    if (secondActive && displayValue != "") {
+        /* values.push(Number(displayValue))
+        operate(values[0],operator,values[1])
+        console.log(values)
+        values = []   */
+        number2 = Number(displayValue)
+        //display.textContent = ""
+
+        operate(number1, operator, number2)
+        //number2 = result
+        //displayValue = "";
+        display.textContent = result
+        firstActive = false
+        secondActive = false
+        thirdActive = true
+
+        override = true
+    }
+    else if (thirdActive && displayValue != "") {
+        operate(number1, operator, number2)
+        resultDisplay.textContent = operate(number1, operator, number2)
+        displayValue = ""
+        number2 = null
+        override = true
+        firstActive = true
+        secondActive = false
+        thirdActive = false
+    }
 })
 
-btn7.addEventListener("click", ()=> {    
-    display.textContent += 7
-    displayValue += "7"
+btn9.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""  // Clear the display screen
+        override = false
+        display.textContent += 9
+        displayValue += "9"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 9
+        displayValue += "9"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn6.addEventListener("click", ()=> {    
-    display.textContent += 6
-    displayValue += "6"
+btn8.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 8
+        displayValue += "8"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 8
+        displayValue += "8"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn5.addEventListener("click", ()=> {    
-    display.textContent += 5
-    displayValue += "5"
+btn7.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 7
+        displayValue += "7"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 7
+        displayValue += "7"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn4.addEventListener("click", ()=> {    
-    display.textContent += 4
-    displayValue += "4"
+btn6.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 6
+        displayValue += "6"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 6
+        displayValue += "6"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn3.addEventListener("click", ()=> {    
-    display.textContent += 3
-    displayValue += "3"
+btn5.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 5
+        displayValue += "5"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 5
+        displayValue += "5"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn2.addEventListener("click", ()=> {    
-    display.textContent += 2
-    displayValue += "2"
+btn4.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 4
+        displayValue += "4"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 4
+        displayValue += "4"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn1.addEventListener("click", ()=> {    
-    display.textContent += 1
-    displayValue += "1"
+btn3.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 3
+        displayValue += "3"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 3
+        displayValue += "3"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
-btn0.addEventListener("click", ()=> {    
-    display.textContent += 0
-    displayValue += "0"
+btn2.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 2
+        displayValue += "2"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 2
+        displayValue += "2"
+        console.log("displayValue:" + displayValue)
+    }
+})
+
+btn1.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 1
+        displayValue += "1"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 1
+        displayValue += "1"
+        console.log("displayValue:" + displayValue)
+    }
+})
+
+btn0.addEventListener("click", () => {
+    if (override) {
+        display.textContent = ""
+        override = false
+        display.textContent += 0
+        displayValue += "0"
+        console.log("displayValue:" + displayValue)
+    } else {
+        display.textContent += 0
+        displayValue += "0"
+        console.log("displayValue:" + displayValue)
+    }
 })
 
 
-btnAC.addEventListener("click", ()=> {
+btnAC.addEventListener("click", () => {
     display.textContent = ""
     displayValue = ""
 })
