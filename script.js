c0824adfd57a46a37b1b43bb0109fa4bd2238f7e
eliptik <adamfeyk5@gmail.com>
2022-12-15 23:58:12 +0300

    Add switched operator cases w bugs

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function add(a, b) {
-    result = Math.round((Number(a) + Number(b))*10000)/10000
+    result = Math.round((Number(a) + Number(b)) * 10000) / 10000
     resultDisplay.textContent = result
 }
 
 function subtrack(a, b) {
-    result = Math.round((Number(a) - Number(b))*10000)/10000
+    result = Math.round((Number(a) - Number(b)) * 10000) / 10000
     resultDisplay.textContent = result
 }
 
 function multiply(a, b) {
-    result = Math.round((Number(a) * Number(b))*10000)/10000
+    result = Math.round((Number(a) * Number(b)) * 10000) / 10000
     resultDisplay.textContent = result
 }
 
 function divide(a, b) {
-    result = Math.round((Number(a) / Number(b))*10000)/10000
+    result = Math.round((Number(a) / Number(b)) * 10000) / 10000
     resultDisplay.textContent = result
 }
 
@@ -55,7 +55,6 @@ const btnSubtrack = document.querySelector("#btnSubtrack")
 const btnMultiply = document.querySelector("#btnMultiply")
 const btnDivide = document.querySelector("#btnDivide")
 
-
 let displayValue = display.textContent
 
 let previousOperator = ""
@@ -67,48 +66,106 @@ let number1 = null
 let number2 = null
 let result = null
 
-
 let override = false
 let isCalculated = false
 let midCalculated = false
 
-let temp = "+"
+let temp = ""
+
+btnAdd.addEventListener("click", decideOperation)
+btnSubtrack.addEventListener("click", decideOperation)
+btnMultiply.addEventListener("click", decideOperation)
+btnDivide.addEventListener("click", decideOperation)
 
 function decideOperation(e) {
     chosenOperator = e.target.textContent
-  
     resultDisplay.textContent = number1 + ` ${chosenOperator} `;
     if (midCalculated == false && temp == chosenOperator) {
-      operation(temp);
-    } else if (midCalculated == false && temp != chosenOperator) {
-      temp = chosenOperator;
-      operation(previousOperator); 
-    } else if (midCalculated == true) {
-      operation(nextOperator);
+        console.log("A")
+        
+        operation(previousOperator);
+    } else  if (midCalculated == false && temp != chosenOperator) {
+        console.log("B")
+        temp = chosenOperator;
+        operation(previousOperator);
+    } 
+    
+    else if (midCalculated == true && temp == chosenOperator) {
+        console.log("C")
+        operation(temp);
+    } else if (midCalculated == true && temp != chosenOperator) {
+        console.log("D")
+        temp = chosenOperator;
+        operation(nextOperator);
     }
 }
 
-btnAdd.addEventListener("click", decideOperation)
-
-btnSubtrack.addEventListener("click", decideOperation)
-
-btnMultiply.addEventListener("click", decideOperation)
-
-btnDivide.addEventListener("click", decideOperation)
-
 function operation(operator) {
-
     //Starting a new calculation:
     if (number1 == null && isCalculated == false) {
+        console.log("11111111")
         number1 = Number(displayValue)
         resultDisplay.textContent = number1 + ` ${chosenOperator} `
         displayValue = ""
         override = true
         previousOperator = chosenOperator
         nextOperator = chosenOperator
+    } 
+    // check if the chosen operator is different from the previous operator
+    else if (number1 != null && number2 == null && displayValue == "" && operator != temp && midCalculated == false) {
+        console.log("changed")
+        temp = operator
+        previousOperator = chosenOperator
+        nextOperator = chosenOperator
+        
+        /* number2 = Number(displayValue)
+        operate(number1, operator, number2)
+        resultDisplay.textContent = result + ` ${chosenOperator} `
+        number1 = result
+        displayValue = ""
+        override = true
+        isCalculated = false
+        midCalculated = true
+        nextOperator = chosenOperator */
     }
     // Do the next calculation using an operator without pressing the equals button:
     else if (number2 == null && displayValue != "") {
+        console.log("22222222")
+        number2 = Number(displayValue)
+        operate(number1, operator, number2)
+        resultDisplay.textContent = result + ` ${chosenOperator} `
+        number1 = result
+        displayValue = ""
+        override = true
+        isCalculated = false
+        midCalculated = true
+        nextOperator = chosenOperator
+    }
+    else if (number1 != null && number2 != null && displayValue == "" && operator != temp && midCalculated == true) {
+        console.log("changed 2")
+        temp = operator
+        previousOperator = chosenOperator
+        nextOperator = chosenOperator
+        
+        midCalculated = false
+    }
+
+
+
+    else if (number2 != null && displayValue != "") {
+        console.log("3333333")
+        number2 = Number(displayValue)
+        operate(number1, operator, number2)
+        resultDisplay.textContent = result + ` ${chosenOperator} `
+        number1 = result
+        displayValue = ""
+        override = true
+        isCalculated = false
+        midCalculated = true
+        nextOperator = chosenOperator
+    }
+    /* else if (number2 == null && displayValue != "") {
+        console.log("aaaaaaaa33333333")
         number2 = Number(displayValue)
         operate(number1, operator, number2)
         resultDisplay.textContent = result + ` ${chosenOperator} `
@@ -120,7 +177,8 @@ function operation(operator) {
         nextOperator = chosenOperator
     }
     // Continue to next calculations using an operator without pressing the equals button:
-    else if (!isCalculated && displayValue != "") {
+    else if (!isCalculated && displayValue != "" && operatorSwitched == true) {
+        console.log("4444444")
         number2 = Number(displayValue)
         operate(number1, operator, number2)
         resultDisplay.textContent = result + ` ${chosenOperator} `
@@ -130,9 +188,10 @@ function operation(operator) {
         isCalculated = false
         midCalculated = true
         nextOperator = chosenOperator
-    }
+    } */
     // If you calculated & got the result and want to use the result for next calculations:
     else if (isCalculated && displayValue == "") {
+        console.log("5555555")
         number1 = result
         resultDisplay.textContent = result + ` ${chosenOperator} `
         isCalculated = false
@@ -158,7 +217,7 @@ btnEquals.addEventListener("click", () => {
     }
 })
 
-function decideNumber(e){
+function decideNumber(e) {
     if (override) {
         display.textContent = ""  // Clear the display screen
         override = false
